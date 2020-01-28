804d1cd2790f6ffa7ca19e6834a7e04bc71045e2
Matheus Roque Petrachin <49207392+MatheusPetrachin@users.noreply.github.com>
2020-01-28 11:01:52 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ document.querySelectorAll('section').forEach(section => {
   }, false)
 })
 
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
+
 AOS.init()
