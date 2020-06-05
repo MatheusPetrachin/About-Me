0725552530e83acedb5ce62a7cc7a0d3b0113c8e
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-06-05 12:26:18 -0300

    Submit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 document.querySelectorAll('section').forEach(section => {
-  section.addEventListener('mouseenter', function() {
-      this.firstElementChild.classList.add('section-hover')
-  }, false)
-  section.addEventListener('mouseleave', function() {
-      this.firstElementChild.classList.remove('section-hover')
-  }, false)
+    section.addEventListener('mouseenter', function() {
+        this.firstElementChild.classList.add('section-hover')
+    }, false)
+    section.addEventListener('mouseleave', function() {
+        this.firstElementChild.classList.remove('section-hover')
+    }, false)
 })
 
 AOS.init()
 
 function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copied: " + copyText.value;
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
 }
 
 function outFunc() {
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copy to clipboard";
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Click to copy my email";
 }
